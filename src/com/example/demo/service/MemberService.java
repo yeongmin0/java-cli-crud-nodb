2937afc7c431da7276e5be71bb4bb57a7f3dc784
yeongmin0 <yeong_00o@naver.com>
2023-10-31 15:59:56 +0900

    10-31

--- src/com/example/demo/service/MemberService.java
+++ src/com/example/demo/service/MemberService.java
@@ -4,14 +4,13 @@ import com.example.demo.domain.Member;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class MemberService {
-    private final List<Member> memberRepository;
+    private final List<Member> memberList;
     private final MemberIdSequenceManager sequenceManager;
 
     public MemberService() {
-        this.memberRepository = new ArrayList<>();
+        this.memberList = new ArrayList<>();
         this.sequenceManager = new MemberIdSequenceManager();
     }
 
@@ -23,27 +22,6 @@ public class MemberService {
 
     public List<Member> getAllMembers() {
         // 변경할 수 없는 사본을 제공.
-        return List.copyOf(memberRepository);
-    }
-
-    // 회원가입할때  리스트내에  닉네임/풀네임이 있는지 없는지 판별하는 기능.
-    public boolean serverUsername(String username) {
-        for (var eachMember : memberRepository) {
-            boolean exists = Objects.equals(eachMember.username, username);// 회원가입시 동일한 유저닉네임이 있을시 false로 판별.
-            if (exists) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean serverNickname(String nickname) {
-        for (var eachMember : memberRepository) {
-            boolean exists = Objects.equals(eachMember.nickname, nickname);
-            if (exists) {
-                return true;
-            }
-        }
-        return false;
+        return List.copyOf(memberList);
     }
 }
--- src/com/example/demo/ui/MainUi.java
+++ src/com/example/demo/ui/MainUi.java
@@ -22,15 +22,12 @@ public class MainUi {
 
     public void run() {
         // 반복문에 Label 달면 -> break, continue 할 때 중첩 반복문 중에서 어느 반복문을 break/continue 할지 선택 가능.
-        mainLoop:
-        while (true) {
+        mainLoop: while (true) {
             switch (path) {
                 case HOME -> home();
                 case SIGN_UP -> signUp();
                 case SIGN_IN -> {
-
                 }
-
                 case SIGN_OUT -> {
                 }
                 case PRINT_ALL_MEMBERS -> memberList();
@@ -48,13 +45,13 @@ public class MainUi {
         int sel;
         System.out.print("""
                 !! Welcome !!
-                                
-                1. 회원가입
-                2. 로그인
-                3. 회원리스트
-                4. 접속중인 리스트
+                
+                1. Sign up
+                2. Login
+                3. Member List
+                4. Who logged in?
                 0. Quit
-                                
+                
                 >\s""");
         sel = scanner.nextInt();
         scanner.nextLine(); // flush buff
@@ -67,7 +64,7 @@ public class MainUi {
             case 0 -> Path.QUIT;
             default -> {
                 System.out.println("Choose one of above numbers.");
-                yield Path.HOME; // switch문의 반환값 같은 역할.
+                yield  Path.HOME; // switch문의 반환값 같은 역할.
             }
         };
     }
@@ -79,29 +76,17 @@ public class MainUi {
 
         System.out.println(" !! Register Your Account !!");
 
-        // do~while를 사용하는 구간.
-        do {
-            System.out.println("Username");
-            System.out.print("> ");
-            username = scanner.nextLine().strip();
-        } while (serverByUsername(username));
-
+        System.out.println("Username");
+        System.out.print("> ");
+        username = scanner.nextLine().strip();
 
         System.out.println("password");
         System.out.print("> ");
         password = scanner.nextLine().strip();
-        do {
-            System.out.println("nickname");
-            System.out.print("> ");
-            nickname = scanner.nextLine().strip();
-        } while (serverBynickname(nickname));
 
-        Member savemember = memberService.signUp(username, password, nickname);
-
-        if (savemember == null) {
-            System.out.println("아이디,비밀번호,닉네임중 존재하는 항목이 있습니다.");
-        }
-
+        System.out.println("nickname");
+        System.out.print("> ");
+        nickname = scanner.nextLine().strip();
 
         // TODO register account with member service.
 
@@ -130,31 +115,5 @@ public class MainUi {
 
         path = Path.HOME;
     }
-
-
-
-    // 회원내에 같은 닉네임/풀네임이 있는지 판별하고 알려주는 기능.
-    private boolean serverByUsername(String username) {
-        boolean serverExists = memberService.serverUsername(username);
-
-        if (serverExists) {
-            System.out.println("이미 닉네임이 존재합니다.");
-        }
-        return serverExists;
-
-
-    }
-
-    private boolean serverBynickname(String nickname) {
-        boolean serverExists = memberService.serverNickname(nickname);
-
-        if (serverExists) {
-            System.out.println("이미 풀네임이 존재합니다.");
-        }
-        return serverExists;
-    }
-
-    // [1] 서버안에서 회원가입을 할시.
-    // [2] 같은 닉네임(완료) , 풀네임 ,이 있을시 true / false 가 나오도록 할 것이다.
 
 }
